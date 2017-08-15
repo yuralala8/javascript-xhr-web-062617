494e28d3e31faaef0e872c8b11fedb4d0e6407ed
yuralala8 <yc1435@nyu.edu>
2017-08-14 21:48:26 -0400

    added eventlistener

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories)
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
+  
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
-function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
-}
 
 
 function getCommits(el) {
